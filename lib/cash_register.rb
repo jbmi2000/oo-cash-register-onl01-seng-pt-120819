aef6e8fe342c1020b5034d3cbc8d3dfeabd6b0e9
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-20 23:32:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,15 +9,16 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    if quantity>1
-          i=0
-          while i<quantity
+    if quantity > 1
+          i = 0
+          while i < quantity
             @items << title
             i+=1
           end
         else
           @items << title
         end
+        @price = price
         @total += price*quantity
         @last_transaction_amount = @total
         @total
