0e510ccb8a6490244816fa6bfd177bba47e2ad04
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-21 01:21:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,7 +36,8 @@ class CashRegister
 end
 
 def void_last_transaction
-   @total -= @last_transaction_amount
+   # @total -= @last_transaction_amount
+   self.total = self.total - self.last_transaction_amount
 end
 
 end
